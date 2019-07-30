0fee2b2fd2ce738a7f23ee1d7072ff9180f76072
unknown <mbellogarindau@gmail.com>
2019-07-30 22:18:53 +0800

    b4 jquery class 2

---
+++ js/myjavascript.js
@@ -0,0 +1,32 @@
+	
+//alert("from external");
+
+	function changeHeading() {
+		alert("you clicked heading ");
+		document.getElementById("heading").innerHTML = " BOOSTRAP ";
+	}
+
+
+ 
+
+	function funcname(param1, parametr2, parametr3){
+		/*
+		function code
+		*/
+
+		return returnvalue;
+
+	}
+
+
+  function  getFactorial (number) {
+  	//calculate factorial
+  	var result = 1;
+  	for (i =1; i<=number; i++) {
+  		result *= i;
+  	}
+
+
+  		return result;
+  }
+
